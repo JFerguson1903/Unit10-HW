1a77bb4fb34b5ff50217e8cecac95273b7da4267
Jessica Ferguson <jferguson1903@gmail.com>
2021-01-29 20:02:16 -0600

    added inquirer loop

--- Develop/app.js
+++ Develop/app.js
@@ -67,7 +67,7 @@ const questions = [{
         type: 'confirm',
         message: 'Would you like to enter another employee?',
         name: 'Employee',
-        choices: ['Yes', 'No'],
+        default: true,
     },
 ];
 
@@ -88,7 +88,7 @@ function init() {
 }
 
 // Function call to initialize app
-init();
+// init();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -109,3 +109,73 @@ init();
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+
+const collectInputs = async(inputs = []) => {
+    const prompts = [{
+            type: 'input',
+            name: 'name',
+            message: "What is the employees name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is their employee id?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the employees email address?',
+        },
+        {
+            type: 'list',
+            message: 'What role is the employee in?',
+            name: 'role',
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the Managers office number?',
+            when: function(answers) {
+                // Only run if user answered Manager to the Role prompt
+                return answers.role === "Manager";
+            },
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the Engineers GitHub Username?',
+            when: function(answers) {
+                // Only run if user answered Engineer to the Role prompt
+                return answers.role === "Engineer";
+            },
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school does the Intern go to?',
+            when: function(answers) {
+                // Only run if user answered Intern to the Role prompt
+                return answers.role === "Intern";
+            },
+        },
+        {
+            type: 'confirm',
+            name: 'employee',
+            message: 'Would you like to enter another employee?',
+            default: true,
+        }
+    ];
+
+    const { employee, ...answers } = await inquirer.prompt(prompts);
+    const newInputs = [...inputs, answers];
+    return employee ? collectInputs(newInputs) : newInputs;
+};
+
+const main = async() => {
+    const inputs = await collectInputs();
+    console.log(inputs);
+};
+
+main();
